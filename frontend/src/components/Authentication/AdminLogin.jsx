d739580f692d8aab732442329fe156aaab703250
nkhanhtung <23020434@vnu.edu.vn>
2025-10-15 08:02:47 +0700

    NKT

---
+++ frontend/src/components/Authentication/AdminLogin.jsx
@@ -0,0 +1,62 @@
+import './UserForm.css'
+import './OverlayPanel.css'
+import './UserLogin.css'
+import axios from 'axios';
+import { useState } from 'react';
+
+export default function AdminLogin() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleAdminLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const formData = new URLSearchParams();
+            formData.append('username', email);
+            formData.append('password', password);
+            // GỌI API ĐĂNG NHẬP CỦA ADMIN
+            const res = await axios.post('http://localhost:8000/admin/login', formData, {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            });
+            // LƯU LẠI TOKEN
+            sessionStorage.setItem('admin_token', res.data.access_token);
+            alert('Đăng nhập vai trò admin thành công!');
+            console.log(res.data);
+        } catch (err) {
+            console.error(err);
+            alert(err.response?.data?.detail || 'Có lỗi xảy ra !');
+        }
+    };
+
+    return (
+        <div className='container'>
+            {/* FORM ĐĂNG NHẬP */}
+            <div className='form-container'>
+                <form onSubmit={handleAdminLogin}>
+                    <h1>Login for admin</h1>
+                    <span>Login now!</span>
+                    <input type='email'
+                           placeholder='Email' 
+                           value={email} 
+                           onChange={(e) => setEmail(e.target.value)} 
+                           required/>
+                    <input type='password' 
+                           placeholder='Password' 
+                           value={password} 
+                           onChange={(e) => setPassword(e.target.value)} 
+                           required/>
+                    <button type='submit'>Sign In</button>
+                </form>
+            </div>
+            {/* OVERLAY */}
+            <div className='toggle-container'>
+                <div className='toggle'>
+                    <div className='toggle-panel toggle-right'>
+                        <h1>Welcome back to CoCoFin</h1>
+                        <p>Sign in with your account, admins!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/components/Authentication/UserForm.jsx
@@ -0,0 +1,76 @@
+import './UserForm.css';
+import axios from 'axios';
+import { useState } from 'react';
+
+const UserForm = ({ type, isActive }) => {
+  const isSignUp = type === 'sign-up';
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (isSignUp) {
+        // GỌI API ĐĂNG KÝ CHO USER
+        const res = await axios.post('http://localhost:8000/user/register', {
+          user_name: username,
+          user_email: email,
+          password_hash: password
+        });
+        alert('Đăng ký tài khoản thành công! Hãy đăng nhập ngay với tài khoản của bạn.');
+        console.log(res.data);
+      } else {
+        // GỌI API ĐĂNG NHẬP CHO USER
+        const formData = new URLSearchParams();
+        formData.append('username', email);
+        formData.append('password', password);
+
+        const res = await axios.post('http://localhost:8000/user/login', formData, {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        });
+
+        // LƯU LẠI TOKEN
+        sessionStorage.setItem('user_token', res.data.current_token);
+        alert('Đăng nhập thành công!');
+        console.log(res.data);
+      }
+    } catch (err) {
+      console.error("API error:", err);
+      console.log("Response data:", err.response?.data);
+      alert(err.response?.data?.detail || err.message || 'Có lỗi xảy ra !');
+    }
+  };
+
+  return (
+    <div className={`form-container ${type} ${isActive && isSignUp ? 'active' : ''}`}>
+      <form onSubmit={handleSubmit}>
+        <h1>{isSignUp ? 'Create Account' : 'Sign In'}</h1>
+        <span>
+          {isSignUp
+            ? 'Start a new account to explore your potential now!'
+            : 'Start your interesting experience with your own account!'}
+        </span>
+        {isSignUp && <input type='text'
+                            placeholder='Username' 
+                            value={username} 
+                            onChange={(e) => setUsername(e.target.value)}
+                            required/>}
+        <input type='email'
+               placeholder='Email'
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
+               required/>
+        <input type='password'
+               placeholder='Password'
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+               required/>
+        <button type='submit'>{isSignUp ? 'Sign Up' : 'Sign In'}</button>
+      </form>
+    </div>
+  );
+};
+
+export default UserForm;
