7e17d16998b0714d369c337a0886b41d1bcec124
Tng20042005 <maiminhtung2005@gmail.com>
2025-11-18 11:18:26 +0700

    websocket: matched_orders, price_alerts, volume_alerts. Spark for price_alerts, volume_alerts

--- backend/main.py
+++ backend/main.py
@@ -20,11 +20,13 @@ app.include_router(visualization.router, prefix="/visualization", tags=["Visuali
 app.include_router(realtime.router, prefix="/realtime", tags=["Realtime Trading"])
 
 origins = [
-    "http://localhost:5173", 
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],  
