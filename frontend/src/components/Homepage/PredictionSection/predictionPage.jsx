781426ca55d898c754b88b00eb1c3b51f2a2a353
nkhanhtung <23020434@vnu.edu.vn>
2025-11-23 12:17:57 +0700

    update

---
+++ frontend/src/components/Homepage/PredictionSection/predictionPage.jsx
@@ -0,0 +1,3 @@
+const PredPage = () => {}
+
+export default PredPage;
--- frontend/src/components/Homepage/navigation_bar.jsx
+++ frontend/src/components/Homepage/navigation_bar.jsx
@@ -95,8 +95,8 @@ const NavBar = () => {
                 <div className="navbar-center">
                     <button className="nav-button" onClick={() => navigate('/homepage/market')}>Thị trường</button>
                     <button className="nav-button" onClick={() => navigate('/homepage/trading')}>Giao dịch</button>
-                    <button className="nav-button">Danh mục</button>
                     <button className="nav-button" onClick={() => navigate('/homepage/chart')}>Biểu đồ</button>
+                    <button className="nav-button" onClick={() => navigate('/homepage/predict')}>AI dự đoán</button>
                 </div>
 
                 {/* Thanh tìm kiếm & nút truy cập hồ sơ tài khoản */}
